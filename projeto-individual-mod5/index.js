5a9d9fb505b7092d5dda26a8c41aa19d03309457
Marlon-Dantas <92075470+Marlon-Dantas@users.noreply.github.com>
2023-01-30 16:41:17 -0300

    Add files via upload

---
+++ projeto-individual-mod5/index.js
@@ -0,0 +1,75 @@
+import inquirer from 'inquirer'
+import chalk from 'chalk'
+import fs from 'fs'
+
+lista()
+
+function lista() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'css',
+            message: 'O seu CSS',
+            choices: [
+                'Adicionar propriedades',
+                'Ver propriedades',
+                'Sair'
+            ],
+        },
+    ])
+        .then((resposta) => {
+            const propriedade = resposta['css']
+
+            if (propriedade === 'Adicionar propriedades') {
+                creatProp()
+            } else if (propriedade === 'Ver propriedades') {
+                verProp()
+            } else if (propriedade === 'Sair') {
+                verProp()
+                console.log(chalk.bgBlue.black('Até a proxima!'))
+                
+            }
+        })
+        .catch((err) => console.log(err))
+}
+
+
+
+function creatProp() {
+    inquirer.prompt([
+        {
+            name: 'prop',
+            message: 'Digite uma propriedade do CSS3:'
+        }
+    ])
+        .then((resposta) => {
+
+            const resp = resposta['prop'];
+            while (resp != 'sair'){
+                
+                fs.appendFile("propriedades.json", `"${resp}":
+`,  (err) => {
+    
+                    if (err) throw err;  
+                });
+            return creatProp() 
+        } 
+         verProp()
+         console.log(chalk.blue('Até a proxima!!'))
+         process.exit
+        }) 
+}
+
+
+function verProp(){
+    var dir = './propriedades.json';
+    fs.readFile('propriedades.json', 'utf8' , (err, data) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log(data)
+      })
+
+var files = fs.readdirSync(dir);
+} 
